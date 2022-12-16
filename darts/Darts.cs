1cf579ae977fed1b36b847948362d428f511048b
Matěj Leifert <32055635+leifert@users.noreply.github.com>
2022-12-16 18:23:08 +0100

    If statements

---
+++ darts/Darts.cs
@@ -0,0 +1,25 @@
+using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        double distance = Math.Sqrt(x * x + y * y);
+        if (distance <= 1.0) //inner circle
+        {
+            return 10;
+        }
+        else if (distance <= 5.0) //middle circle
+        {
+            return 5;
+        }
+        else if (distance <= 10.0) //outer circle
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
