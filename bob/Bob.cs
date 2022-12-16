e3b816dadee08c5f922083ba7412bf566f01573f
Matěj Leifert <32055635+leifert@users.noreply.github.com>
2022-12-16 17:39:55 +0100

    Extension methods

---
+++ bob/Bob.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Linq;
+
+public static class Bob
+{
+    public static string Response(string statement)
+    {
+        if (string.IsNullOrEmpty(statement) || statement.All(char.IsWhiteSpace))
+        {
+            return "Fine. Be that way!";
+        }
+        else if (!statement.Any(char.IsLower) && statement[statement.Length-1] == '?' && statement.Any(char.IsLetter))
+        {
+            return "Calm down, I know what I'm doing!";
+        }
+        else if(statement[statement.Length-1] == '?' || statement[statement.Length-4] == '?')
+        {
+            return "Sure.";
+        }
+        
+        else if (!statement.Any(char.IsLower) && statement.Any(char.IsLetter))
+        {
+            return "Whoa, chill out!";
+        }
+        else {
+            return "Whatever.";
+        }
+    }
+
+    /*
+      public static string Response(string message)
+    {
+        if (message.IsSilence())
+            return "Fine. Be that way!";
+        if (message.IsYell() && message.IsQuestion())
+            return "Calm down, I know what I'm doing!";
+        if (message.IsYell())
+            return "Whoa, chill out!";
+        if (message.IsQuestion())
+            return "Sure.";
+        return "Whatever.";
+    }
+    private static bool IsSilence(this string message) =>
+        string.IsNullOrWhiteSpace(message);
+    private static bool IsYell(this string message) =>
+        message.Any(char.IsLetter) && message.ToUpperInvariant() == message;
+    private static bool IsQuestion(this string message) =>
+        message.TrimEnd().EndsWith("?");
+}
+     */
+ 
+}
---
+++ log-analysis/LogAnalysis.cs
@@ -0,0 +1,20 @@
+using System;
+
+public static class LogAnalysis 
+{
+    public static string SubstringAfter(this string str, string delimeter){
+        return str[(str.IndexOf(delimeter)+delimeter.Length)..];
+    }
+
+     public static string SubstringBetween(this string str, string delimeter1,string delimeter2){ 
+        return str[(str.IndexOf(delimeter1)+delimeter1.Length)..str.IndexOf(delimeter2)];  
+     }
+    
+    public static string Message(this string str){ 
+         return str[(str.IndexOf(":")+2)..]; 
+     }
+
+    public static string LogLevel(this string str) {
+        return str[(str.IndexOf("[")+1)..str.IndexOf("]")]; 
+    }
+}
