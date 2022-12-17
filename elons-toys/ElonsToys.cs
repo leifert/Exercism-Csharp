3a3266a32c626ac00d73fde75c1726fb31000f5b
Matěj Leifert <32055635+leifert@users.noreply.github.com>
2022-12-17 21:35:55 +0100

    Classes

---
+++ elons-toys/ElonsToys.cs
@@ -0,0 +1,29 @@
+using System;
+
+class RemoteControlCar
+{
+    public int Distance { get; set; } = 0;
+    public int Battery { get; set; } = 100;
+
+    public static RemoteControlCar Buy()
+    {
+        return new RemoteControlCar();
+    }
+
+    public string DistanceDisplay()=>$"Driven {Distance} meters";
+    
+
+    public string BatteryDisplay()=> (Battery > 0)? $"Battery at {Battery}%": EmptyBattery();
+
+    public void Drive()
+    {
+        if (Battery >= 1) {
+            Distance += 20;
+            Battery -= 1;
+        }
+        else {
+            EmptyBattery();
+        }
+    }
+    private string EmptyBattery()=>"Battery empty";
+}
