5647c736397708f32a41352b76bea0ec8769639c
Matěj Leifert <32055635+leifert@users.noreply.github.com>
2022-12-19 22:50:09 +0100

    String Builder

---
+++ squeaky-clean/SqueakyClean.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Text;
+
+public static class Identifier
+{
+    public static string Clean(string identifier)
+    {
+        StringBuilder sb= new StringBuilder();
+        bool kebabFlag = false;
+
+        foreach (char c in identifier) {
+            if (kebabFlag) {
+                sb.Append(char.ToUpper(c));
+                kebabFlag = false;
+            }
+            else {
+                switch (c) {
+                    case char x when char.IsWhiteSpace(c):
+                        sb.Append('_'); 
+                        break;
+                    case char x when char.IsControl(c):
+                        sb.Append("CTRL"); 
+                        break;
+                    case '-':
+                        kebabFlag = true;
+                        break;
+                    case char x when c >= 945 && c <= 969:
+                        break;
+                    case char x when char.IsLetter(c):
+                        sb.Append(c);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        return sb.ToString();
+    }
+}
