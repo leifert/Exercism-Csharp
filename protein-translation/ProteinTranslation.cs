7c29c4622733cfd4124cdbedfec644132dc842a9
Matěj Leifert <32055635+leifert@users.noreply.github.com>
2022-12-24 22:53:04 +0100

    LINQ

---
+++ protein-translation/ProteinTranslation.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ProteinTranslation
+{
+    public static string[] Proteins(string strand)
+    {
+        Dictionary<string,string> proteins  = new Dictionary<string, string>(){
+            {"AUG", "Methionine"},
+            {"UUU", "Phenylalanine"},
+            {"UUC", "Phenylalanine"},
+            {"UUA", "Leucine"},
+            {"UUG", "Leucine"},
+            {"UCU", "Serine"},
+            {"UCC", "Serine"},
+            {"UCA", "Serine"},
+            {"UCG", "Serine"},
+            {"UAU", "Tyrosine"},
+            {"UAC", "Tyrosine"},
+            {"UGU", "Cysteine"},
+            {"UGC", "Cysteine"},
+            {"UGG", "Tryptophan"},
+            {"UAA", "STOP"},
+            {"UAG", "STOP"},
+            {"UGA", "STOP"},
+         };
+
+        return Enumerable.Range(0,strand.Length/3)
+            .Select(i => strand.Substring(i*3,3))
+            .Select(c => proteins[c])
+            .TakeWhile(p => p!="STOP").ToArray();
+    }
+}
