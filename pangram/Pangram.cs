689ac89e98da22cebff4eec3745c1346ec612355
Matěj Leifert <32055635+leifert@users.noreply.github.com>
2022-12-24 23:12:25 +0100

    Strings

---
+++ pangram/Pangram.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Linq;
+
+public static class Pangram
+{
+    private static string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    public static bool IsPangram(string input) => alphabet.All(c => input.ToLower().Contains(c));//input.ToLower().Contains
+}
